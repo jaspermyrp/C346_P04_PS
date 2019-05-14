9ce90b939938d394df6af8ccfc658c977e0caccc
Jasper <17020202@myrp.edu.sg>
2019-05-14 15:31:46 +0800

    Commit

--- app/src/main/java/sg/edu/rp/c346/reservation/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/reservation/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.reservation;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -11,6 +12,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,37 +57,7 @@ public class MainActivity extends AppCompatActivity {
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = etName.getText().toString();
-                String pax = etPax.getText().toString();
-                String mobile = etMobile.getText().toString();
-                String smoking;
-
-                if (name.isEmpty() || pax.isEmpty() || mobile.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Please fill in all fields!", Toast.LENGTH_SHORT).show();
-                } else {
-                    int paxNo = Integer.parseInt(pax);
-
-                    if(paxNo < 1 || mobile.length() != 8){
-                        String errorMsg = "Invalid Pax or Mobile number";
-                        Toast.makeText(MainActivity.this, errorMsg, Toast.LENGTH_SHORT).show();
-                    }else{
-                        // Reserve //
-
-                        // Check checkbox selection //
-                        if (cbSmokingArea.isChecked()) {
-                            smoking = "Smoking Area";
-                        } else {
-                            smoking = "Non-smoking Area";
-                        }
-
-                        // Check dates //
-                        String date = dp.getDayOfMonth() + "/" + dp.getMonth() + "/" + dp.getYear();
-                        String time = tp.getCurrentHour() + ":" + tp.getCurrentMinute();
-
-                        String reservationMsg = String.format("Reservation successful\nName:%s, Pax:%s, Mobile:%s, \n%s \non %s %s ", name, pax, mobile, smoking, date, time);
-                        Toast.makeText(MainActivity.this, reservationMsg, Toast.LENGTH_LONG).show();
-                    }
-                }
+                callReservation();
             }
         });
 
@@ -97,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
     }
 
     public void setDefaultDateTime() {
@@ -111,5 +88,46 @@ public class MainActivity extends AppCompatActivity {
         etPax.setText("");
         etMobile.setText("");
         cbSmokingArea.setChecked(false);
+    }
+
+    public void callReservation() {
+        String name = etName.getText().toString();
+        String pax = etPax.getText().toString();
+        String mobile = etMobile.getText().toString();
+        String smoking;
+
+        if (name.isEmpty() || pax.isEmpty() || mobile.isEmpty()) {
+            Toast.makeText(MainActivity.this, "Please fill in all fields!", Toast.LENGTH_SHORT).show();
+        } else {
+            int paxNo = Integer.parseInt(pax);
+
+            if (paxNo < 1 || mobile.length() != 8) {
+                String errorMsg = "Invalid Pax or Mobile number";
+                Toast.makeText(MainActivity.this, errorMsg, Toast.LENGTH_SHORT).show();
+            } else {
+                // Reserve //
+
+                // Check checkbox selection //
+                if (cbSmokingArea.isChecked()) {
+                    smoking = "Smoking Area";
+                } else {
+                    smoking = "Non-smoking Area";
+                }
+
+                // Check dates //
+                String date = dp.getDayOfMonth() + "/" + (dp.getMonth() + 1) + "/" + dp.getYear();
+                String time = tp.getCurrentHour() + ":" + tp.getCurrentMinute();
+
+                // Check if date is earlier than current date //
+                //if () {
+                //  Toast.makeText(MainActivity.this, "The reservation date cannot be less than today!", Toast.LENGTH_LONG).show();
+                //} else {
+                String reservationMsg = String.format("Reservation successful\nName:%s, Pax:%s, Mobile:%s, \n%s \non %s %s ", name, pax, mobile, smoking, date, time);
+                Toast.makeText(MainActivity.this, reservationMsg, Toast.LENGTH_LONG).show();
+                //}
+
+
+            }
+        }
     }
 }
